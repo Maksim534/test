2b27ce7bd8ba5756185b22cf8a7bfe34ca55cc41
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-02-08 16:40:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS users (
 )''')
 
 async def clickal(message):
+    user_id = message.from_user.id
     cursor.execute(f'UPDATE users SELECT balance + 1 WHERE user_id = ?', (user_id,))
                    
 
@@ -55,7 +56,7 @@ async def balance(message: types.Message):
 
 @dp.message_handler(commands=['click'])
 async def click(message: types.Message):
-    await clickal(message.from_user.id)
+    await clickal
     await message.answer(f'Вы заработали + 1 рубаааль', parse_mode='html')
 
 
